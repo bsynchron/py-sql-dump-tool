7dee1ad9d1d3a7c6b61efb92ded078bb80b66660
florian.patzwall <florian.patzwall@localhost.localdomain>
2020-04-07 06:05:09 -0400

    some basic struct

---
+++ lib/sql.py
@@ -0,0 +1,16 @@
+#!/bin/python3.6
+
+def setup():
+    DB_HOST="localhost"
+    DB_USER="root"
+    DB_USER_PASS=""
+
+
+    DB_NAME="to_backup"
+    main()
+
+def main():
+    print("Started")
+
+if(__name__ == "lib.sql"):
+    setup()
---
+++ main.py
@@ -0,0 +1,42 @@
+#!/bin/python3.6
+import os
+import lib.sql
+
+
+def setup():
+    print("Setup")
+    BACKUP_PATH="/backup"
+    os.mkdir(BACKUP_PATH)
+    print("Setup successful")
+    main()
+
+def main():
+    print("Main")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(__name__ == "__main__"):
+    setup()
